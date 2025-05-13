7fd22522f1614e2b20b3319947a4b89cb635f1a7
jpovalles <jpovalles@javerianacali.edu.co>
2025-05-13 12:53:38 -0500

    some styles in the solicitudes table

--- src/pages/operario/Pedidos.jsx
+++ src/pages/operario/Pedidos.jsx
@@ -34,7 +34,9 @@ function Pedidos() {
                     <td>12:15 am</td>
                     <td>Biblioteca</td>
                     <td>Edificio Palmas</td>
-                    <td><div className="menu-icon">☰</div></td>
+                    <td className="optionContainer">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="white" height="2em" viewBox="0 0 24 24" className="optionIcon"><path d="M12,10a2,2,0,1,1-2,2A2,2,0,0,1,12,10ZM4,14a2,2,0,1,0-2-2A2,2,0,0,0,4,14Zm16-4a2,2,0,1,0,2,2A2,2,0,0,0,20,10Z"/></svg>
+                    </td>
                 </tr>
             ))}
         </tbody>
