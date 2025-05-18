c36be6a857b1d740b6cb0eb6929a3a5bbc206149
jpovalles <jpovalles@javerianacali.edu.co>
2025-05-17 23:12:44 -0500

    Refactor user menu in NavBar components to retrieve user data from localStorage and implement logout functionality

--- src/pages/admin/NavBarAdmin.jsx
+++ src/pages/admin/NavBarAdmin.jsx
@@ -5,15 +5,20 @@ import { Link, useMatch, useResolvedPath } from "react-router-dom";
 
 function NavBarAdmin({}){
 
-    const user = {
-        nombreUser: "Sebastian Enriquez",
-        rolUser: "Administrador"
-    }
-    const {nombreUser, rolUser} = user;
+    const nombreUser = localStorage.getItem("usuario_actual");
+    const username = localStorage.getItem("username");
+    const rolUser = "Administrador";
 
     const [mostrarMenu, setMostrarMenu] = useState(false);
 
     const toggleMenu = () => setMostrarMenu(!mostrarMenu);
+
+    const handleLogout = () => {
+        localStorage.removeItem("token");  // Elimina el token de autenticación
+        localStorage.removeItem("rol");    // Elimina el rol del usuario
+        localStorage.removeItem("username");    // Elimina el usuario
+        window.location.href = "/";   // Redirige al usuario al login
+    };
 
     return(
         <div className="navbarAdmin">
@@ -44,11 +49,12 @@ function NavBarAdmin({}){
             {mostrarMenu && (
                 <div className="user-menu">
                     <p><strong>Nombre: </strong>{nombreUser}</p>
+                    <p><strong>Usuario: </strong>{username}</p>
                     <p><strong>Rol: </strong> {rolUser}</p>
                     <p className="user-menu-separator">
                         <hr/>
                     </p>
-                    <button className="logout-btn">Cerrar sesión</button>
+                    <button className="logout-btn" onClick={() => handleLogout()}>Cerrar sesión</button>
                 </div>
             )}
         </div>
--- src/pages/operario/NavbarOperario.jsx
+++ src/pages/operario/NavbarOperario.jsx
@@ -5,15 +5,20 @@ import { Link, useMatch, useResolvedPath } from "react-router-dom";
 
 function NavbarOperario({}){
 
-    const user = {
-        nombreUser: "Juan Pablo Ospina",
-        rolUser: "Operario"
-    }
-    const {nombreUser, rolUser} = user;
+    const nombreUser = localStorage.getItem("usuario_actual");
+    const username = localStorage.getItem("username");
+    const rolUser = "Operario";
 
     const [mostrarMenu, setMostrarMenu] = useState(false);
 
     const toggleMenu = () => setMostrarMenu(!mostrarMenu);
+
+    const handleLogout = () => {
+        localStorage.removeItem("token");  // Elimina el token de autenticación
+        localStorage.removeItem("rol");    // Elimina el rol del usuario
+        localStorage.removeItem("username");    // Elimina el usuario
+        window.location.href = "/";   // Redirige al usuario al login
+    };
 
     return(
         <div className="navbarOperario">
@@ -38,11 +43,12 @@ function NavbarOperario({}){
             {mostrarMenu && (
                 <div className="user-menu">
                     <p><strong>Nombre: </strong>{nombreUser}</p>
+                    <p><strong>Usuario: </strong>{username}</p>
                     <p><strong>Rol: </strong> {rolUser}</p>
                     <p className="user-menu-separator">
                         <hr/>
                     </p>
-                    <button className="logout-btn">Cerrar sesión</button>
+                    <button className="logout-btn" onClick={() => handleLogout()}>Cerrar sesión</button>
                 </div>
             )}
         </div>
